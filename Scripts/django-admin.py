93cc3823d8846607df2c11dd7c833e47a8e22302
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-09-05 23:53:07 +0530

    changes

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\raghav\desktop\myproj~1\projec~1\qaskap~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
